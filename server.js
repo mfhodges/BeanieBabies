8492657b00d4f9876a72c542f620e87bfddec6cb
mfhodges <mfhodges95@gmail.com>
2020-05-09 20:37:50 -0400

    created apollo server and start script runs client and server now

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 //server.js
+//var sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+import { graphqlExpress,graphiqlExpress,} from 'graphql-server-express';
+import bodyParser from 'body-parser';
+import { schema } from './src/schema';
+
+
+
+
+// enable ssl redirect
+//app.use(sslRedirect());
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -14,4 +24,8 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
 app.listen(port);
---
+++ src/schema.js
@@ -0,0 +1,14 @@
+// src/schema.js
+import {
+    makeExecutableSchema,
+    addMockFunctionsToSchema, # we'll use this later
+  } from 'graphql-tools';const typeDefs = `type Channel {
+     id: ID!                # "!" denotes a required field
+     name: String
+  }# This type specifies the entry points into our API. In this case
+  # there is only one - "channels" - which returns a list of channels.
+  type Query {
+     channels: [Channel]    # "[]" means this is a list of channels
+  }
+  `;const schema = makeExecutableSchema({ typeDefs });
+  export { schema };
