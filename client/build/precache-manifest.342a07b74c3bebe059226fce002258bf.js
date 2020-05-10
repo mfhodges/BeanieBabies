aaf9e8c53e67a5e76079224fe01ed9161490834e
mfhodges <mfhodges95@gmail.com>
2020-05-10 12:37:59 -0400

    resolved package script issues.  to run project `npm start` in root

---
+++ client/build/precache-manifest.342a07b74c3bebe059226fce002258bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c0da945180247bbc4874d2ed1fd535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69f1572ded67a6258a9d",
+    "url": "/static/css/main.8589dc8e.chunk.css"
+  },
+  {
+    "revision": "b2051fa62927cc153168",
+    "url": "/static/js/2.98c29782.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.98c29782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f1572ded67a6258a9d",
+    "url": "/static/js/main.6511636c.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
