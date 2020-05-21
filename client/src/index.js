4a62cdd939274f564de3d266dfaaf82e9e15e5e0
mfhodges <mfhodges95@gmail.com>
2020-05-21 19:14:31 -0400

    testing if mongo will work on heroku

--- client/src/index.js
+++ client/src/index.js
@@ -12,8 +12,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 /// How will this connect when in production?
 const httpLink = createHttpLink({
-  //uri: 'http://localhost:5000/graphql/'
-  uri:'/graphql'
+  uri: 'http://localhost:5000/graphql/'
+  //uri:'/graphql'
 })
 // if we are in production we should do 
 // uri: '/graphql'
